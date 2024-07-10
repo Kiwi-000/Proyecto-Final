4006645c28dd6802f2d9364aa50a0a69884efd1a
Kiwi-000 <antoniatadea283@gmail.com>
2024-07-09 23:29:16 -0400

    Update enfermedad.py

--- enfermedad.py
+++ enfermedad.py
@@ -1,10 +1,4 @@
-
 class Enfermedad:
-    def __init__(self, infeccion_probable, promedio_pasos, tasa_recuperacion):
+    def __init__(self, infeccion_probable, tasa_recuperacion):
         self.infeccion_probable = infeccion_probable
-        self.promedio_pasos = promedio_pasos
         self.tasa_recuperacion = tasa_recuperacion
-
-        def tasa_recuperacion():
-            # Podria ser metodo SIR
-            pass
