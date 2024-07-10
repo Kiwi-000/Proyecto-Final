6ad2338bd1404bf238798139714db62b5e4bff5f
Kiwi-000 <antoniatadea283@gmail.com>
2024-07-09 23:39:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import pandas as pd
 from simulador import Simulador
+from enfermedad import Enfermedad
 
 def main():
-    #comunidades 
+    infeccion_probable= 0.3  
+    tasa_recuperacion = 0.1   
 
-    #comunidad micro
-    pass
+    enfermedad = Enfermedad(infeccion_probable, tasa_recuperacion)
+    simulador = Simulador(num_dias=30, enfermedad=enfermedad)
 
-
-
+if __name__ == "__main__":
+    main()
 
 
 
