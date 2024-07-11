5a53dbc7f27b0d1e740ef7d557ac118220ca62ec
Kiwi-000 <antoniatadea283@gmail.com>
2024-07-11 14:16:01 -0400

    Update comunidad.py

--- comunidad.py
+++ comunidad.py
@@ -11,7 +11,6 @@ class Comunidad:
         df = pd.read_csv(csv_file)
         print("~~~~~~~~~")
 
-        #nota: usar numpy en este arreglo ~ chatgpts
         personas = []
         for index, row in df.iterrows():
             persona = Persona(row['id'], row['nombre'], row['apellido'], row["estado"])
@@ -22,21 +21,18 @@ class Comunidad:
         print("\nSe crearon correctamente 500 personas")
 
         #se imprimen las personas
-        i = 0
+        '''
         for persona in personas:
-            print(f"{persona.id} ~ {persona.nombre} ~ {persona.estado}")
-            i += 1
-        
+            print(f"{persona.id} ~ {persona.nombre} - {persona.apellido}~ {persona.estado}")
+           '''
         return personas
 
     def infectar_aleatoriamente(self, num_personas, personas):
         #se agarra la cantidad de num_personas de las 500 en personas
         personas_infectadas = random.sample(personas, num_personas)
-
         for persona in personas_infectadas:
             print(persona.id)
             persona.infectar(self.enfermedad)
-
         return personas
 
     def crear_comunidad_dia(self, num_personas):
