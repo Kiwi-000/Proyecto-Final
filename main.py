ebe82f209734991bdf9986cd3a213eaa8aaf85fc
Kiwi-000 <antoniatadea283@gmail.com>
2024-07-11 14:18:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ from simulador import Simulador
 from enfermedad import Enfermedad
 
 def main():
-    num_dias = 30
     tasa_transmision= 0.3  
     tasa_recuperacion = 5
 
-    #def __init__(self, num_dias, tasa_transmision, promedio_pasos, tasa_recuperacion):
     covid = Enfermedad(tasa_transmision, tasa_recuperacion)
 
-    simulador = Simulador(num_dias=30, enfermedad=covid)
+    simulador = Simulador(num_dias=50, enfermedad=covid)
 
     simulador.ejecutar_simulacion()
 
 if __name__ == "__main__":
     main()
+  
   
 
 
