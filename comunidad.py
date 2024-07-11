e9c906c5eec39b418724b6c3bd1e1e6dc0dc6b5f
Kiwi-000 <antoniatadea283@gmail.com>
2024-07-11 01:32:04 -0400

    Update comunidad.py
    edit comunidad

--- comunidad.py
+++ comunidad.py
@@ -3,22 +3,43 @@ import pandas as pd
 from persona import Persona
 
 class Comunidad:
-    def __init__(self,personas,num_personas, enfermedad):
-        self.personas = []
-        self.num_personas = num_personas
+    def __init__(self, enfermedad):
         self.enfermedad = enfermedad
+        self.personas = self.leer_personas('personas.csv')
 
     def leer_personas(self, csv_file):
         df = pd.read_csv(csv_file)
-        for i, row in df.iterrows():
-            persona = Persona(i, row['nombre'], row['apellido'])
-            self.personas.append(persona)
+        print("~~~~~~~~~")
 
-    def infectar_aleatoriamente(self, num_personas):
-        for _ in range(num_personas):
-            persona = random.choice(self.personas)
+        #nota: usar numpy en este arreglo ~ chatgpts
+        personas = []
+        for index, row in df.iterrows():
+            persona = Persona(row['id'], row['nombre'], row['apellido'], row["estado"])
+            personas.append(persona)
+
+        #se escoge una persona al azar para enfermarla
+        personas = self.infectar_aleatoriamente(1, personas)
+        print("\nSe crearon correctamente 500 personas")
+
+        #se imprimen las personas
+        i = 0
+        for persona in personas:
+            print(f"{persona.id} ~ {persona.nombre} ~ {persona.estado}")
+            i += 1
+        
+        return personas
+
+    def infectar_aleatoriamente(self, num_personas, personas):
+        #se agarra la cantidad de num_personas de las 500 en personas
+        personas_infectadas = random.sample(personas, num_personas)
+
+        for persona in personas_infectadas:
+            print(persona.id)
             persona.infectar(self.enfermedad)
+
+        return personas
 
     def crear_comunidad_dia(self, num_personas):
         return random.sample(self.personas, num_personas)
-
+    
+#talca = Comunidad("enfermedad")
