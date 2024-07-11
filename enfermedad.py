c3444bebba8da9ed5018f4bf73adb14303d193dd
Kiwi-000 <antoniatadea283@gmail.com>
2024-07-11 14:31:16 -0400

    Update enfermedad.py

--- enfermedad.py
+++ enfermedad.py
@@ -1,4 +1,17 @@
 class Enfermedad:
-    def __init__(self, infeccion_probable, tasa_recuperacion):
-        self.infeccion_probable = infeccion_probable
+    def __init__(self, tasa_transmision, tasa_recuperacion):
+        #probabilidad de contagio
+        self.tasa_transmision = tasa_transmision
+        #cuantos dias va a estar enfermo hasta que se recupere
         self.tasa_recuperacion = tasa_recuperacion
+
+    #get
+    def get_tasa_transmision(self):
+        return self.tasa_transmision 
+    
+    def get_tasa_recuperacion(self):
+        return self.tasa_recuperacion
+    
+
+    #set
+    
