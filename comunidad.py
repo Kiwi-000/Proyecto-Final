20fbc86597c87f95b48d99eba41f20580864f466
Kiwi-000 <antoniatadea283@gmail.com>
2024-07-12 02:49:32 -0400

    Update comunidad.py
    Comentarios

--- comunidad.py
+++ comunidad.py
@@ -7,6 +7,9 @@ class Comunidad:
         self.enfermedad = enfermedad
         self.personas = self.leer_personas('personas.csv')
 
+#-----------------------------------------------------------------------------------------------------------------------------------
+    #crear personas
+
     def leer_personas(self, csv_file):
         df = pd.read_csv(csv_file)
 
@@ -15,18 +18,24 @@ class Comunidad:
             persona = Persona(row['id'], row['nombre'], row['apellido'], row["estado"])
             personas.append(persona)
 
-        #se escoge una persona al azar para enfermarla
-        personas = self.infectar_aleatoriamente(1, personas)
+        #se infecta persona del arreglo personas
+        personas = self.infectar_aleatoriamente(1, personas) #se puede cambiar la cantidad de personas a infectar
         return personas
+
+#-----------------------------------------------------------------------------------------------------------------------------------
+    #metodo para infectar personas
 
     def infectar_aleatoriamente(self, num_personas, personas):
         #se agarra la cantidad de num_personas de las 500 en personas
         personas_infectadas = random.sample(personas, num_personas)
         for persona in personas_infectadas:
-            print(persona.id)
+            print(persona.id) #imprimir id de la persona elegida
             persona.infectar(self.enfermedad)
         return personas
 
+    #metodo para crear una sub comunidad
+
     def crear_comunidad_dia(self, num_personas):
         return random.sample(self.personas, num_personas)
+    
     
