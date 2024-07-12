8a6f14ad6ab880df7f25ab2cfdf4d11fde903767
Kiwi-000 <antoniatadea283@gmail.com>
2024-07-12 02:47:57 -0400

    Add files via upload
    Este archivo main está diseñado para evaluar el código únicamente desde la terminal, ignorando la versión principal que incluye la interfaz gráfica. Esto se debe a que la interfaz gráfica aún presenta errores. De esta manera, mientras se trabaja la interfaz gráfica, se puede comprobar que las otras clases sigan funcionando correctamente utilizando este archivo main.

---
+++ main_terminal.py
@@ -0,0 +1,122 @@
+import pandas as pd
+from simulador import Simulador
+from enfermedad import Enfermedad
+
+def main():
+    tasa_transmision= 0.3  
+    tasa_recuperacion = 5
+
+    covid = Enfermedad(tasa_transmision, tasa_recuperacion)
+
+    simulador = Simulador(num_dias=50, enfermedad=covid)
+
+    simulador.ejecutar_simulacion()
+
+if __name__ == "__main__":
+    main()
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+3
