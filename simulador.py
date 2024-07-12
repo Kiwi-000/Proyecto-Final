1af9e7e539f826881e4b72d748f050b718c6558d
Kiwi-000 <antoniatadea283@gmail.com>
2024-07-12 00:24:45 -0400

    Update simulador.py

--- simulador.py
+++ simulador.py
@@ -10,21 +10,20 @@ class Simulador:
         self.enfermedad = enfermedad
         self.resultados = [] 
 
-    #iniciar la simulacion de propagacion 
+    #Ejecutar la simulacion de propagacion 
     def ejecutar_simulacion(self):
 
         for dia in range(1, self.num_dias):
-            comunidad_dia = self.comunidad.crear_comunidad_dia(50)
+            comunidad_dia = self.comunidad.crear_comunidad_dia(50) #Crear sub comunidades
 
+            #Funciones que deben iterar sobre cada dia
             self.propagacion_enfermedad(comunidad_dia)
             self.recuperar_persona(self.comunidad.personas)
             self.guardar_comunidad(dia, self.comunidad.personas)
 
             for persona in self.comunidad.personas:
-                #print(persona.id, persona.estado)
                 if persona.estado == "i":
                     persona.contador += 1
-                    #print( persona.contador, persona.id )
 
         print("\n Datos finales de la simulacion\n")
     
@@ -35,7 +34,9 @@ class Simulador:
         self.calcular_infectados_totales()
         self.calcular_recuperados_totales()
 
-    #Propagar gente 
+
+
+    #Propagar enfermedad en la comunidad del dia
     def propagacion_enfermedad(self, comunidad_dia):
         for persona in comunidad_dia:
             if persona.estado == 'i':
@@ -43,8 +44,7 @@ class Simulador:
                     if otra_persona.estado == 's' and random.random() < self.enfermedad.tasa_transmision:
                         otra_persona.infectar(self.enfermedad)
     
-
-    #tratar de recuperar gente 
+    # Recuperacion de las personas 
     def recuperar_persona(self, comunidad):
         for persona in comunidad:
             if persona.estado == 'i':
@@ -59,6 +59,7 @@ class Simulador:
         infectados = len([p for p in comunidad if p.estado == 'i'])
         recuperados = len([p for p in comunidad if p.estado == 'r'])
         print(f"Día {dia}: Suceptibles={suceptibles}, Infectados={infectados}, Recuperados={recuperados}")
+        return suceptibles, infectados , recuperados
 
 #____________________________________________________________________________________________________________________________
     #Porcentajes finales de toda la comunidad
@@ -75,6 +76,8 @@ class Simulador:
         porcentaje_suceptible = (suceptibles_totales / total_personas) * 100
         print (f"El {porcentaje_suceptible} % nunca se enfermo")    
 
+        return porcentaje_suceptible
+
 
     # calcular promedio infectados en toda la simulacion 
     def calcular_infectados_totales(self):
@@ -88,6 +91,8 @@ class Simulador:
         porcentaje_infectados = (infectados_totales / total_personas)* 100
         print (f"Quedaron enfermos el {porcentaje_infectados} %")
 
+        return porcentaje_infectados
+
 
     # calcular preomedio recuperados en toda la simulasion 
     def calcular_recuperados_totales(self):
@@ -100,6 +105,8 @@ class Simulador:
 
         porcentaje_recuperadas = (recuperados_totales / total_personas)* 100
         print (f"Está recuperado el {porcentaje_recuperadas}%")    
+
+        return porcentaje_recuperadas
 
 
 
